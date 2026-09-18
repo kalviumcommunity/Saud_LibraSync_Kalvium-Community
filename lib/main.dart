e4d81a324a3b20dbb67f021b1e6dac1d3ff869d8
SaudKhanAbbas <saudkhanabbas@gmail.com>
2026-09-18 15:47:19 +0530

    feat: initialize Firebase services

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
+import 'firebase_options.dart';
 import 'screens/home_screen.dart';
 import 'theme/app_theme.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const LibraSyncApp());
 }
 
